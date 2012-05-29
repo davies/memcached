895ac45c5ecbe9fe5022d7b2e953a5ee7c894521
Davies Liu <davies.liu@gmail.com>
2012-05-29 12:16:06 +0800

    enable maxconns_fast by default

--- memcached.c
+++ memcached.c
@@ -220,7 +220,7 @@ static void settings_init(void) {
     settings.backlog = 1024;
     settings.binding_protocol = negotiating_prot;
     settings.item_size_max = 1024 * 1024; /* The famous 1MB upper limit. */
-    settings.maxconns_fast = false;
+    settings.maxconns_fast = true;
     settings.hashpower_init = 0;
     settings.slab_reassign = false;
     settings.slab_automove = false;
